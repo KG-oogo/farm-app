8a6f0bf4471597e7eafdb90781b7b465fd21bac1
molepoka <molepoka@webmail.co.za>
2020-05-22 01:44:32 +0200

    Updates

---
+++ precache-manifest.f1edd78cca9c5c0b4fd6e4f0e89b3ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b752469adc4e4b489d1811f3d533a03",
+    "url": "/farm-app/index.html"
+  },
+  {
+    "revision": "adbf612734713b401cad",
+    "url": "/farm-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f3a68265fcbb3fdda07",
+    "url": "/farm-app/static/js/2.ed0b62d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/farm-app/static/js/2.ed0b62d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbf612734713b401cad",
+    "url": "/farm-app/static/js/main.86dcd9ec.chunk.js"
+  },
+  {
+    "revision": "f85ea64344f449f912eb",
+    "url": "/farm-app/static/js/runtime-main.8c32ec1b.js"
+  },
+  {
+    "revision": "a2c0ea49e8d20e58cb4e5aaea635a8b2",
+    "url": "/farm-app/static/media/MacNutTree.a2c0ea49.jpg"
+  }
+]);
